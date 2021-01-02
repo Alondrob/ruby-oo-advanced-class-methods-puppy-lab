b551ba0d1ec319b6c21d0f711bcac70f8b39e20a
YOUR_GITHUB_USER_NAME <YOUR_GITHUB_EMAIL_ADDRESS>
2021-01-02 15:11:50 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,39 @@
-# Add your code here
+class Dog
+    @@all = []
+
+    
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        # @@all << self
+        self.save
+    end
+
+
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all = []
+    end
+
+    def self.print_all
+        @@all.each {|dog| puts dog.name}
+    end
+
+    def save
+        @@all << self
+    end
+end
+
+   #[pluto,fido,maddy]
+
+    dog_1 = Dog.new("pluto")
+    dog_2 = Dog.new("Fido")
+    dog_3 = Dog.new("Maddy")
+
+    p Dog.all
+  
